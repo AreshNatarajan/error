8e1a0197897fdf4656a9e1171aa4c10c6f37eef8
AreshNatarajan <areshn18@gmail.com>
2024-12-03 18:16:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// let myFistName = 'Aresh'  => declare varible
+
+// if(myFistName){
+//     console.log(myFistName);   
+//     let myLastName = 'kumar'  => declare variable
+// }
+// console.log(myLastName);      => let is a block scope variable ! htis code gives "  myLastname is not defined "
+
+
+// let myname  = 'aresh'
+// if(true){
+//     let myname = 'kumar'  re-declare 
+//     console.log(myname);
+// }
+// console.log(myname);
+
+
+
+
